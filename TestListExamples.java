0805ddaaf35376ffba72b3e064d0c1eb97f949a7
steventang5 <stt012@ucsd.edu>
2023-11-27 14:25:48 -0800

    Created grade bash

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,84 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test
+  public void testFilter_AllStringsValid() {
+    List<String> inputList = Arrays.asList("apple", "banana", "cherry");
+    StringChecker sc = s -> true; 
+    List<String> filteredList = ListExamples.filter(inputList, sc);
+    assertEquals(Arrays.asList("apple", "banana", "cherry"), filteredList);
+  }
+
+  @Test
+  public void testFilter_NoStringsValid() {
+    List<String> inputList = Arrays.asList("apple", "banana", "cherry");
+    StringChecker sc = s -> false; 
+    List<String> filteredList = ListExamples.filter(inputList, sc);
+    assertTrue(filteredList.isEmpty());
+  }
+
+  @Test
+  public void testFilter_SomeStringsValid() {
+    List<String> inputList = Arrays.asList("apple", "banana", "cherry");
+    StringChecker sc = s -> s.startsWith("a"); 
+    List<String> filteredList = ListExamples.filter(inputList, sc);
+    assertEquals(Arrays.asList("apple"), filteredList);
+  }
+
+  @Test
+  public void testFilter_EmptyList() {
+    List<String> inputList = Arrays.asList();
+    StringChecker sc = s -> true;
+    List<String> filteredList = ListExamples.filter(inputList, sc);
+    assertTrue(filteredList.isEmpty());
+  }
+
+    @Test
+    public void testMerge_BothListsEmpty() {
+      List<String> list1 = Arrays.asList();
+      List<String> list2 = Arrays.asList();
+      List<String> result = ListExamples.merge(list1, list2);
+      assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void testMerge_FirstListEmpty() {
+      List<String> list1 = Arrays.asList();
+      List<String> list2 = Arrays.asList("apple", "banana");
+      List<String> result = ListExamples.merge(list1, list2);
+      assertEquals(Arrays.asList("apple", "banana"), result);
+    }
+
+    @Test
+    public void testMerge_SecondListEmpty() {
+      List<String> list1 = Arrays.asList("apple", "banana");
+      List<String> list2 = Arrays.asList();
+      List<String> result = ListExamples.merge(list1, list2);
+      assertEquals(Arrays.asList("apple", "banana"), result);
+    }
+
+    @Test
+    public void testMerge_BothListsNonEmpty() {
+      List<String> list1 = Arrays.asList("apple", "cherry");
+      List<String> list2 = Arrays.asList("banana", "date");
+      List<String> result = ListExamples.merge(list1, list2);
+      assertEquals(Arrays.asList("apple", "banana", "cherry", "date"), result);
+    }
+
+    @Test
+    public void testMerge_WithDuplicateStrings() {
+      List<String> list1 = Arrays.asList("apple", "banana");
+      List<String> list2 = Arrays.asList("banana", "cherry");
+      List<String> result = ListExamples.merge(list1, list2);
+      assertEquals(Arrays.asList("apple", "banana", "banana", "cherry"), result);
+    }
+
+    @Test
+    public void testMerge_WithAllDuplicates() {
+      List<String> list1 = Arrays.asList("apple", "apple");
+      List<String> list2 = Arrays.asList("apple", "apple");
+      List<String> result = ListExamples.merge(list1, list2);
+      assertEquals(Arrays.asList("apple", "apple", "apple", "apple"), result);
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,3 +1,4 @@
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
@@ -7,6 +8,26 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+
+file='student-submission/ListExamples.java'
+
+if [[ -f $file ]]
+then
+    echo 'Correct File'
+else
+    echo 'Wrong File'
+fi
+
+cp $file grading-area
+cp TestListExamples.java grading-area
+
+cd grading-area
+
+#Compile and run
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar ListExamples.java TestListExamples.java
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > test_output_file
+
+
 
 
 # Draw a picture/take notes on the directory structure that's set up after
